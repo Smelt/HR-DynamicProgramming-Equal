26aa7ae9ca661a8a81e4687ea46406596074b125
Smelt <smelting103@gmail.com>
2017-01-28 17:47:11 -0500

    Trying to solve with a 1D Array

--- src/com/Solution.java
+++ src/com/Solution.java
@@ -8,7 +8,50 @@ public class Solution {
 	
 	
 	 public static void main(String[] args) {
-	        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+	        //first we take in input
+		 	Scanner sc = new Scanner(System.in);
+		 	int testCases = sc.nextInt();
+		 	
+		 	
+		 	for(int i = 0; i < testCases; i++){
+		 		int internCount = sc.nextInt();
+		 		int[] interns = new int[internCount];
+		 
+		 		for(int x = 0; x < internCount; x++ ){
+		 			interns[x] = sc.nextInt();
+		 		}
+		 		process(interns);
+		 
+		 	}
+		 	
 	    }
+	 
+	 /**
+	  * this method takes in the Array and processes it
+	  * @param interns
+	  */
+	 private static void process(int[] interns){
+		 
+		 if( checkEqual(interns)){
+			 System.out.println("0");
+		 }
+		 int max = 0;
+		 for(int i = 0; i < interns.length; i++){
+			 
+		 }
+	 }
+	 /**
+	  *This method checks to see if the interns already have an equal amount of chocolate
+	  */
+	 private static boolean checkEqual(int[] interns){
+		 int check = interns[0];
+		 
+		 for(int i = 0; i < interns.length; i++){
+			 if(check != interns[i]){
+				 return false;
+			 }
+		 }
+		 return true;
+	 }
 
 }
